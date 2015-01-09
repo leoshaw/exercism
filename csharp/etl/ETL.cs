798df7fd62825508d699c2fa04ab57606324095e
Leo Shaw <leo@leoshaw.co.uk>
2015-01-09 12:29:36 +0000

    Added ETL

---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class ETL
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> input)
+    {
+        var result = new Dictionary<string, int>();
+
+        foreach (var pair in input)
+        {
+            var score = pair.Key;
+            var letters = pair.Value;
+            foreach (var letter in letters)
+            {
+                result.Add(letter.ToLower(), score);
+            }
+        }
+
+        return result;
+    }
+}
